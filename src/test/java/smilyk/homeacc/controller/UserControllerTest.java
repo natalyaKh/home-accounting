b914db77d53fbbfb1c42c801b62d03ad04760411
smilyk <smilyk1982@gmail.com>
2021-01-17 12:23:54 +0200

    add testing createUser  instruct method from testing

---
+++ src/test/java/smilyk/homeacc/controller/UserControllerTest.java
@@ -0,0 +1,33 @@
+package smilyk.homeacc.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void getUserByUuid() {
+    }
+
+    @Test
+    void getUsers() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
+    @Test
+    void verifyEmailToken() {
+    }
+}
--- src/test/java/smilyk/homeacc/service/user/UserServiceImplTest.java
+++ src/test/java/smilyk/homeacc/service/user/UserServiceImplTest.java
@@ -4,9 +4,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import smilyk.homeacc.dto.UserDto;
+import smilyk.homeacc.dto.VerificationMailDto;
 import smilyk.homeacc.exceptions.HomeaccException;
 import smilyk.homeacc.model.User;
 import smilyk.homeacc.repo.UserRepository;
@@ -19,9 +22,10 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class UserServiceImplTest {
+    ModelMapper modelMapper = new ModelMapper();
     String userUuid = "1111";
     String encryptedPassword = "1234";
     String emailVerificationToken = "12345";
@@ -68,24 +72,25 @@ class UserServiceImplTest {
 //        userRepository.save(userEntity);
         when(userRepository.save(any(User.class))).thenReturn(user);
 
-        UserDto userDto = UserDto.builder()
-                .email(email)
-                .firstName(userFirstName)
-                .lastName(userLastName)
-                .userUuid(userUuid)
-                .password(password)
-                .build();
+        UserDto userDto = modelMapper.map(user, UserDto.class);
         UserDto storedUserDto = userService.createUser(userDto);
 
         assertNotNull(storedUserDto);
         assertEquals(user.getFirstName(), storedUserDto.getFirstName());
+        assertNotNull(storedUserDto.getUserUuid());
+//        checking how times called method
+        verify(utils, times(1)).generateUserUuid();
+        verify(bCryptPasswordEncoder, times(1)).encode(encryptedPassword);
+        verify(userRepository, times(1)).save(any(User.class));
+//        instruct method from testing
+        Mockito.doNothing().when(mailService).sendVerificationEmailMail(any(VerificationMailDto.class));
 
     }
 
     @Test
     void testGetUserByUserUuid() {
 
-        Optional<User> returnCacheValue = Optional.of( user);
+        Optional<User> returnCacheValue = Optional.of(user);
         when(userRepository.findByUserUuidAndDeleted(anyString(), eq(false)))
                 .thenReturn(returnCacheValue);
         UserDto userDto = userService.getUserByUserUuid(userUuid);
