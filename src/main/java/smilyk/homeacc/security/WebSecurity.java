0cab95bbe56b16a29f48ec14566349d8cffcab79
smilyk <smilyk1982@gmail.com>
2021-01-21 18:04:57 +0200

    making  all api Stateless

--- src/main/java/smilyk/homeacc/security/WebSecurity.java
+++ src/main/java/smilyk/homeacc/security/WebSecurity.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import smilyk.homeacc.constants.SecurityConstants;
@@ -27,7 +28,9 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .permitAll().anyRequest().authenticated()
         .and()
                 .addFilter(getAuthenticationFilter())
-        .addFilter(new AuthorizationFilter(authenticationManager()));
+        .addFilter(new AuthorizationFilter(authenticationManager()))
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
 //    @Override
