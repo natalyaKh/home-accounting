d4b6f816dfe05970efa041c665058b75da0bc086
smilyk <smilyk1982@gmail.com>
2021-01-21 18:01:42 +0200

    create checking methods by token

---
+++ src/main/java/smilyk/homeacc/security/AuthorizationFilter.java
@@ -0,0 +1,64 @@
+package smilyk.homeacc.security;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import smilyk.homeacc.constants.SecurityConstants;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * onlu user with token has possibility to change his email and/or password
+ * только тот, кто вошел в приложение имеет вохможность менять свое имя и/или пароль.
+ * choosing token for checking if user wants to change his credentials
+ * используем токен, что бы убедиться, что пользователь меняет свои данные
+ */
+public class AuthorizationFilter extends BasicAuthenticationFilter {
+    public AuthorizationFilter(AuthenticationManager authManager) {
+        super(authManager);
+    }
+    @Override
+    protected void doFilterInternal(HttpServletRequest req,
+                                    HttpServletResponse res,
+                                    FilterChain chain) throws IOException, ServletException {
+
+        String header = req.getHeader(SecurityConstants.HEADER_STRING);
+        if (header == null || !header.startsWith(SecurityConstants.TOKEN_PREFIX)) {
+            chain.doFilter(req, res);
+            return;
+        }
+
+        UsernamePasswordAuthenticationToken authentication = getAuthentication(req);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        chain.doFilter(req, res);
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+        String token = request.getHeader(SecurityConstants.HEADER_STRING);
+
+        if (token != null) {
+
+            token = token.replace(SecurityConstants.TOKEN_PREFIX, "");
+
+            String user = Jwts.parser()
+                    .setSigningKey( SecurityConstants.getTokenSecret() )
+                    .parseClaimsJws( token )
+                    .getBody()
+                    .getSubject();
+
+            if (user != null) {
+                return new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
+            }
+            return null;
+        }
+        return null;
+    }
+
+}
--- src/main/java/smilyk/homeacc/security/WebSecurity.java
+++ src/main/java/smilyk/homeacc/security/WebSecurity.java
@@ -26,7 +26,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
                 .permitAll().anyRequest().authenticated()
         .and()
-                .addFilter(getAuthenticationFilter());
+                .addFilter(getAuthenticationFilter())
+        .addFilter(new AuthorizationFilter(authenticationManager()));
     }
 
 //    @Override
