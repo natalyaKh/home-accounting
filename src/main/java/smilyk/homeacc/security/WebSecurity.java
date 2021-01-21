05a4825494b831c606516fb1e0181f8e6247c0d4
smilyk <smilyk1982@gmail.com>
2021-01-21 18:10:13 +0200

    code review

--- src/main/java/smilyk/homeacc/security/WebSecurity.java
+++ src/main/java/smilyk/homeacc/security/WebSecurity.java
@@ -1,12 +1,10 @@
 package smilyk.homeacc.security;
 
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import smilyk.homeacc.constants.SecurityConstants;
 import smilyk.homeacc.service.user.UserService;
