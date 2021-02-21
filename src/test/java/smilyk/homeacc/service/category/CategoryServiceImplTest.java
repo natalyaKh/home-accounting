b207002cf1959a2074c9a0a05c06a084eb559b93
smilyk <smilyk1982@gmail.com>
2021-02-21 17:17:07 +0200

    add test category sybcategory service

--- src/test/java/smilyk/homeacc/service/category/CategoryServiceImplTest.java
+++ src/test/java/smilyk/homeacc/service/category/CategoryServiceImplTest.java
@@ -2,17 +2,61 @@ package smilyk.homeacc.service.category;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import smilyk.homeacc.dto.CategoryDto;
+import smilyk.homeacc.enums.CategoryType;
+import smilyk.homeacc.model.Category;
+import smilyk.homeacc.repo.CategoryRepository;
+import smilyk.homeacc.utils.Utils;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 class CategoryServiceImplTest {
+    ModelMapper modelMapper = new ModelMapper();
+    private Category category;
+    @InjectMocks
+    CategoryServiceImpl categoryService;
+    @Mock
+    CategoryRepository categoryRepository;
+    @Mock
+    Utils utils;
 
     @BeforeEach
     void setUp() {
+        MockitoAnnotations.initMocks(this);
+        category = Category.builder()
+            .categoryName("Products")
+            .categoryUuid("1111")
+            .userUuid("2222")
+            .deleted(false)
+            .description("")
+            .type(CategoryType.OUTPUT)
+            .build();
     }
 
     @Test
-    void save() {
-//        TODO
+    void testCreateCategory() {
+        when(utils.generateUserUuid()).thenReturn(UUID.randomUUID());
+        when(categoryRepository.save(any(Category.class))).thenReturn(category);
+
+        CategoryDto categoryDto = modelMapper.map(category, CategoryDto.class);
+        Category storedCategory = categoryService.save(categoryDto);
+
+        assertNotNull(storedCategory);
+        assertEquals(category.getCategoryName(), storedCategory.getCategoryName());
+        assertNotNull(storedCategory.getCategoryUuid());
+        assertNotNull(storedCategory.getUserUuid());
+
+        verify(utils, times(1)).generateUserUuid();
+        verify(categoryRepository, times(1)).save(any(Category.class));
+
     }
 }
--- src/test/java/smilyk/homeacc/service/subcategory/SubcategoryServiceImplTest.java
+++ src/test/java/smilyk/homeacc/service/subcategory/SubcategoryServiceImplTest.java
@@ -2,17 +2,61 @@ package smilyk.homeacc.service.subcategory;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import smilyk.homeacc.dto.SubcategoryDto;
+import smilyk.homeacc.model.Category;
+import smilyk.homeacc.model.Subcategory;
+import smilyk.homeacc.repo.SubcategoryRepository;
+import smilyk.homeacc.utils.Utils;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 class SubcategoryServiceImplTest {
+    ModelMapper modelMapper = new ModelMapper();
+    private Subcategory subcategory;
+    @InjectMocks
+    SubcategoryServiceImpl subcategoryService;
+    @Mock
+    SubcategoryRepository subcategoryRepository;
+    @Mock
+    Utils utils;
+
 
     @BeforeEach
     void setUp() {
+        MockitoAnnotations.initMocks(this);
+        subcategory = Subcategory.builder()
+            .subcategoryUuid("1111")
+            .subcategoryName("dress")
+            .deleted(false)
+            .userUuid("2222")
+            .description("this is subcategory")
+            .build();
     }
 
     @Test
     void save() {
-//        TODO
+        when(utils.generateUserUuid()).thenReturn(UUID.randomUUID());
+        when(subcategoryRepository.save(any(Subcategory.class))).thenReturn(subcategory);
+
+        SubcategoryDto subcategoryDto = modelMapper.map(subcategory, SubcategoryDto.class);
+        Subcategory storedSubcategory = subcategoryService.save(subcategoryDto);
+
+        assertNotNull(storedSubcategory);
+        assertEquals(subcategory.getSubcategoryName(), storedSubcategory.getSubcategoryName());
+        assertNotNull(storedSubcategory.getSubcategoryUuid());
+        assertNotNull(storedSubcategory.getUserUuid());
+
+        verify(utils, times(1)).generateUserUuid();
+        verify(subcategoryRepository, times(1)).save(any(Subcategory.class));
+
     }
 }
